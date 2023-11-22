2a7cf54f883fdef96a6e3229a79dd3670f93f4c6
MaicolCampochiaro <maicolcampochiaro@gmail.com>
2023-11-22 14:25:05 +0100

    Active storage, cloudinary

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -73,7 +73,7 @@ class ArtworksController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def artwork_params
-    params.require(:artwork).permit(:title, :description, :user_id)
+    params.require(:artwork).permit(:title, :description, :image, :user_id)
   end
 
   # Setting the query for the where clause
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,4 +2,5 @@ class Artwork < ApplicationRecord
   belongs_to :user
   has_many :artwork_tags
   has_many :tags, through: :artwork_tags
+  has_one_attached :image
 end
